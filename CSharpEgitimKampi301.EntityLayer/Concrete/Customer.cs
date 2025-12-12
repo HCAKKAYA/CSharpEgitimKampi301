3501dba8d82eb10e98f5caf4e3a909153b982c38
Huseyin Can Akkaya <hcaakkaya@gmail.com>
2025-12-13 00:16:49 +0300

    Update

--- CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
+++ CSharpEgitimKampi301.EntityLayer/Concrete/Customer.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management.Instrumentation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,13 +10,12 @@ namespace CSharpEgitimKampi301.EntityLayer.Concrete
     public class Customer
     {
         public int CustomerId { get; set; }
-
         public string CustomerName { get; set; }
         public string CustomerSurname { get; set; }
         public string CustomerDistrict { get; set; }
         public string CustomerCity { get; set; }
-
         public List<Order> Orders { get; set; }
+        public bool CustomerStatus { get; set; }
 
     }
 }
